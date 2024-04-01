b327cab9262ccdd7dd8a22b1284a1f5cd07f6f34
Poisonlocket <loribono@gmx.ch>
2024-04-01 20:55:17 +0200

    putting spinning globe on hold, will continiue with stack grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,61 @@
-
+"use client"
+import Typewriter from 'typewriter-effect';
 import React, {Fragment} from 'react'
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
-import Link from 'next/link';
-import Globerender from "@/components/ui/globe";
-import {AuroraBackground} from "@/components/ui/aurora-background";
-import {CanvasRevealEffect} from "@/components/ui/canvas-reveal-effect";
-import {TracingBeam} from "@/components/ui/tracing-beam";
+
+
+
 import {CanvasRevealFood} from "@/components/ui/reveal_versions/canvas-reveal-food";
 import {CanvasRevealSecurity} from "@/components/ui/reveal_versions/canvas-reveal-security";
 import {CanvasRevealCoding} from "@/components/ui/reveal_versions/canvas-reveal-it";
-import {TypewriterEffect} from "@/components/ui/typewriter";
-import App from "next/app";
+import "./globals.css"
+import {CardHoverEffectDemo} from "@/components/ui/card-hover-effect";
+
 
 export default function page() {
     return (
         <div className="min-h-full bg-black">
-            <Globerender></Globerender>
+      
             <div className="h-96">
 
                 <div className="mx-[8%]">
-                <div className="text-[16em] font-semibold">
+                <div className="lg:text-[16em] md:text-[12em] sm:text-[8em] xs:text-[4em] font-semibold">
                     Hey
                 </div>
 
-                <div className="lg:text-[11em] md:text-[8em] bg-black flex w-screen">
-                    I&apos;m @<TypewriterEffect words={[{text:"Test", className: "text-blue-500 text-[4.8em]"}]} cursorClassName="h-96]"></TypewriterEffect>
+                <div className="lg:text-[8em] md:text-[4em] sm:text-[3em] xs:text-[2em] flex HoverEffect">
+                    I&apos;m <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter.typeString(` <span style="
+                background:linear-gradient(45deg, #1FD1F9, #B621FE);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                "
+                >‎ A Full Stack Developer</span>`)
+                            .pauseFor(2500)
+                            .deleteAll()
+                        typewriter.typeString(`<span style="
+                background:linear-gradient(45deg, #1FD1F9, #B621FE);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                "
+                >‎ an IT-Student</span>`)
+                            .pauseFor(2500)
+                            .deleteAll()
+                        typewriter.typeString(`<span style="
+                background:linear-gradient(45deg, #1FD1F9, #B621FE);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                "
+                >‎ @Nonno</span>`)
+                            .start();
+                    }}
+                />
                 </div>
                 </div>
             <div className="h-96">
+                <div className=" font-semibold">
 
+                </div>
             </div>
             <div className="grid mx-[4%] lg:grid-cols-3 md:grid-cols-2">
                 <CanvasRevealSecurity></CanvasRevealSecurity>
@@ -41,8 +63,12 @@ export default function page() {
                 <CanvasRevealFood></CanvasRevealFood>
 
             </div>
+                <div className={"text-center lg:text-[8em] md:text-[6em] sm:text-[4em] xs:text-[2em] font-semibold"}>
+                    &lt; My Stack / &gt;
+                </div>
+                <CardHoverEffectDemo>
 
-
+                </CardHoverEffectDemo>
             </div>
         </div>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,4 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const svgToDataUri = require("mini-svg-data-uri");
-const colors = require("tailwindcss/colors");
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
-
-
-
-
-
-
 import type { Config } from "tailwindcss"
-
-
 
 const config = {
   darkMode: ["class"],
@@ -24,19 +10,26 @@ const config = {
 	],
   prefix: "",
   theme: {
+    screens:{
+      "xs": "240px",
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px"
+    },
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+
     },
+
     extend: {
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
+        background: "black",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
@@ -88,39 +81,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), addVariablesForColors, function ({ matchUtilities, theme }: any) {
-    matchUtilities(
-        {
-          "bg-grid": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-grid-small": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-dot": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-    );
-  },
-  ],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
 export default config
