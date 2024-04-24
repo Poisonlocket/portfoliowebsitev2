ebd078e8a7d60bf15687b7df6cb389ea8b2765e6
Poisonlocket <loribono@gmx.ch>
2024-04-24 18:29:21 +0200

    fixed some issue with react not liking apostrophs

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function page() {
                             <div className="timeline-end lg:text-xl mb-10">
                                 <time className="font-mono italic">September 2023</time>
                                 <div className="text-lg font-black">Baselhack</div>
-                                iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has been the primary part of Apple's consumer desktop offerings since its debut in August 1998, and has evolved through seven distinct forms
+                                test1
                             </div>
                             <hr />
                         </li>
@@ -127,7 +127,7 @@ export default function page() {
                             <div className="timeline-start md:text-end mb-10">
                                 <time className="font-mono italic">February 2024</time>
                                 <div className="text-lg font-black">Cyber Security Events</div>
-                                The iPod is a discontinued series of portable media players and multi-purpose mobile devices designed and marketed by Apple Inc. The first version was released on October 23, 2001, about 8+1⁄2 months after the Macintosh version of iTunes was released. Apple sold an estimated 450 million iPod products as of 2022. Apple discontinued the iPod product line on May 10, 2022. At over 20 years, the iPod brand is the oldest to be discontinued by Apple
+                                test2
                             </div>
                             <hr />
                         </li>
@@ -139,7 +139,7 @@ export default function page() {
                             <div className="timeline-end mb-10">
                                 <time className="font-mono italic">April 2024</time>
                                 <div className="text-lg font-black">Accessibility Hack</div>
-                                iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone accounts for 15.6% of global smartphone market share
+                                test3
                             </div>
                             <hr />
                         </li>
